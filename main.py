74cd880ff9322affcc5a85ac0f30befd6698d408
TheTurkeyDev <turkey@theturkey.dev>
2025-02-09 13:01:54 -0500

    Print out certain packet info for testing

--- main.py
+++ main.py
@@ -7,12 +7,30 @@ con.parity = 'N'  # No parity
 con.stopbits = 1  # Number of Stop bits = 1
 con.timeout = 0.001
 
-str = ''
 
-
-def handle_str(packet_str):
-    if packet_str.startswith("f607"):
-        print("07")
+def handle_packet(header, length, data):
+    if length == '22':
+        n = 2
+        # This is not effecient. Really should use the byte array and not the hex string
+        parts = [data[i:i+n] for i in range(0, len(data), n)]
+        print("tt: " + parts[0])
+        print("uu: " + parts[1])
+        print("uu: " + parts[1])
+        print("pp: Counter?: " + parts[5])
+        print("aa: Home Score: " + parts[9])
+        print("bb: Away Score: " + parts[10])
+        print("cc: Home Shots: " + parts[11])
+        print("dd: Away Shots: " + parts[12])
+        print("ee: Home Goal: " + parts[13])
+        print("ff: Away Goal: " + parts[14])
+        print("gg: Period: " + parts[15])
+        print("hh: H Pen 1: " + parts[25])
+        print("ii: H Pen 2: " + parts[26])
+        print("jj: A Pen 1: " + parts[27])
+        print("kk: A Pen 2: " + parts[28])
+        print("ll: H Pen Ind: " + parts[29])
+        print("mm: A Pen Ind: " + parts[30])
+        print("yy: Checksum?: " + parts[32])
 
 
 try:
@@ -22,7 +40,8 @@ try:
             lengthHex = con.read(1).hex()
             length = int(lengthHex, 16)
             data = con.read(length - 1).hex()
-            print("f6" + lengthHex + data)
+            print(header + lengthHex + data)
+            handle_packet(header, lengthHex, data)
         else:
             print("None f6 recieved! " + header)
 except KeyboardInterrupt:
